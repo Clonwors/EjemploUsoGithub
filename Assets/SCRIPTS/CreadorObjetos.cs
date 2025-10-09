1c69d715b7980920a90806056287675041e1eae9
Hector A. <clonworas@gmail.com>
2025-10-09 12:09:22 +0100

    Se crean esferas recio
    Ahora se crean en lugares aleatorios esferas

--- Assets/SCRIPTS/CreadorObjetos.cs
+++ Assets/SCRIPTS/CreadorObjetos.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class CreadorObjetos : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    GameObject[] objetoQueVoyACrear;
+
+    [SerializeField]
+    int cantidadDeObjetos = 5;
+
     void Start()
     {
-        
+        Debug.Log(Random.Range(-3f, 3f));
+        //Crear 5 veces el mismo objeto, cambiando su posicion de forma aleatoria
+
+        for (int i = 0; i < cantidadDeObjetos; i++)
+        {
+            float posX = Random.Range(-5f, 6f);
+            float posY = Random.Range(0f, 8f);
+            float posZ = Random.Range(-7f, 13f);
+            int numAleatorio = Random.Range(0, objetoQueVoyACrear.Length);
+            Instantiate(objetoQueVoyACrear[numAleatorio], new Vector3(posX, posY, posZ), Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
 }
+ 
